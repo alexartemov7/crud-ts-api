46a0ffc1cb7bc4717a26e6c81f4c0f01775e0161
Alex Artemov <artemov.sandro@gmail.com>
2023-11-13 17:14:01 -0500

    finished API CRUD

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 //1. Import packages
 import express from 'express'
 import cors from 'cors'
-import {MongoClient, ObjectId} from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import 'dotenv/config'
+import bcrypt from 'bcrypt'
 //2. Use packages
 const app = express()
 app.use(cors())
@@ -10,8 +11,8 @@ app.use(cors())
 app.use(express.json())
 
 const client = new MongoClient(process.env.MONGO_URI as string)
-   const db = client.db('dinos-store')
-   const users = db.collection('users')
+const db = client.db('dinos-store')
+const users = db.collection('users')
 
 client.connect()
 
@@ -20,13 +21,16 @@ client.connect()
 app.listen(process.env.PORT, () => console.log(`App is listening on port 8080`))
 //4. Create a get endpoint
 app.get('/', async (req, res) => {
-   const allUsers = await users.find().toArray()
+    const allUsers = await users.find().toArray()
     res.send(allUsers)
 })
 
 //5. Create an end point to create a user
-app.post('/', async(req, res) => {
-    const userAdded = await users.insertOne(req.body)
+app.post('/', async (req, res) => {
+    const userEmail = req.body.email
+    const userPassword = req.body.password
+    const hashPass = await bcrypt.hash(req.body.password, 10)
+    const userAdded = await users.insertOne({ email: userEmail, password: hashPass })
     res.status(201).send(userAdded)
 })
 
@@ -34,7 +38,7 @@ app.post('/', async(req, res) => {
 app.delete('/:_id', async (req, res) => {
     const cleanId = new ObjectId(req.params._id)
     console.log('req.params -->', req.params)
-    const userDeleted = await users.findOneAndDelete({ _id: cleanId})
+    const userDeleted = await users.findOneAndDelete({ _id: cleanId })
     res.send(userDeleted)
 })
 
@@ -43,6 +47,22 @@ app.patch('/:_id', async (req, res) => {
     const cleanId = new ObjectId(req.params._id)
 
     const userUpdated = await users.findOneAndUpdate(
-        { _id: cleanId}, { $set: req.body})
+        { _id: cleanId }, { $set: req.body })
     res.send(userUpdated)
+})
+
+//8. login endpoint
+app.post('/login', async (req, res) => {
+
+    const userPassword = req.body.password
+    const foundUser = await users.findOne({ email: req.body.email })
+
+    if (foundUser) {
+        const passInDb = foundUser?.password
+        const result = await bcrypt.compare(userPassword, passInDb)
+        console.log('result -> ', result)
+        res.send(foundUser)
+    } else {
+        res.json('User not found!!! X')
+    }
 })
